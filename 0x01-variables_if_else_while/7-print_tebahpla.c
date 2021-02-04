5ef34d1d9b7dbf2d0dda9858424a99d4d39e5429
Julianeme <juliantve@gmail.com>
2021-02-04 22:43:12 +0000

    alphabet backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+for (x = 'z'; x >= 'a'; x--)
+	{
+	putchar(x);
+	}
+putchar(10);
+return (0);
+}
