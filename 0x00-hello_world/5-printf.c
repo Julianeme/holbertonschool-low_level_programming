02cf63afc58f6c8804da30de535654265e74e2da
Julianeme <juliantve@gmail.com>
2021-02-04 00:25:00 +0000

    Printf command

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-
+ #include <stdio.h>
+  2 /**
+  3  * main - prints a string
+  4  * Return: 0
+  5  */ 
 int main(void)
 
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-  return (0);
+	return (0);
 }
 
