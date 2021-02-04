e3d89b02b0c99cdc28cd0972e99844f1cae757f7
Julianeme <juliantve@gmail.com>
2021-02-04 22:32:32 +0000

    prints single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+for (x = '0'; x <= '9'; x++)
+	{
+	putchar(x);
+	}
+putchar(10);
+return (0);
+}
