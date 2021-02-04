b55c22612ed7dfea58d42d61cbc82b500ad1d501
Julianeme <juliantve@gmail.com>
2021-02-04 22:52:46 +0000

    hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+for (x = 48; x <= 57; x++)
+	putchar(x);
+for (x = 97; x <= 102; x++)
+	putchar(x);
+
+putchar(10);
+return (0);
+}
