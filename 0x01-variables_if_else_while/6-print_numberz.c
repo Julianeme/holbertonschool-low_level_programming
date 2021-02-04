ec5d89592c82162fb4390df656fdf6083f4ea2af
Julianeme <juliantve@gmail.com>
2021-02-04 22:37:03 +0000

    prints numbers 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+for (x = 48; x <= 57; x++)
+	{
+	putchar(x);
+	}
+putchar(10);
+return (0);
+}
