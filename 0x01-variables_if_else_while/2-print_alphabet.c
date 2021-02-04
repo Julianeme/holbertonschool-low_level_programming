fa621b24d3eb8be1aaa3e27884d30cb5978ef5f8
Julianeme <juliantve@gmail.com>
2021-02-04 21:46:45 +0000

    print the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+	putchar(x);
+	putchar(',');
+	}
+return (0);
+}
