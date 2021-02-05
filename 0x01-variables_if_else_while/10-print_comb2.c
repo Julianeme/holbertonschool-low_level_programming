5cda3fb359d7568a58b99ccca5ff3b543a630be8
Julianeme <juliantve@gmail.com>
2021-02-05 00:34:56 +0000

    00-99

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -15,9 +15,10 @@ for (x = 48; x <= 57; x++)
 		putchar(x);
 		putchar(z);
 		if (x && z != 57)
-		putchar(44);
-		putchar(32);
-	}
+		{
+			putchar (44);
+			putchar(32);
+		}
 	}
 putchar(10);
 return (0);
