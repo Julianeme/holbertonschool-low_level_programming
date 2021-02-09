0ae0d92a466e8f32a97a3397b0d95081e451517d
Julianeme <juliantve@gmail.com>
2021-02-09 13:54:44 +0000

    comments correction

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,19 @@
 #include "holberton.h"
 
 /**
- * Check if a letter is lower case.
+ *_isalpha - descripción: Check if a letter is alphabetic characters.
  *
- * Return: Always 0.
+ *short description: Takes a  letter and verifies if it is an
+ * alphabetic character.
+ *
+ *@c: descripcion - Stores the letter to be verified.
+ *
+ *Return: 1 if the letter is alpha case or 0 in any other case.
  */
 
 int _isalpha(int c)
 {
-	if(c >= 97 && c <= 122 || c >= 65 && c <= 90)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
