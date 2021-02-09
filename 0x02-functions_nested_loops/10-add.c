00d117fb4f75694ab656bda5930f8046847dd748
Julianeme <juliantve@gmail.com>
2021-02-09 20:19:57 +0000

    a + b program

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *add - descripción: adds two numbers.
+ *
+ *short description: takes two integer numbers, adds them
+ *and returns the result
+ *
+ *@x: int to be added
+ *@y: ints used to store the variables to be added.
+ *
+ *Return: the value of the addition
+ */
+
+int add(int x, int y)
+{
+	int res;
+
+	res = x + y;
+	return (res);
+}
