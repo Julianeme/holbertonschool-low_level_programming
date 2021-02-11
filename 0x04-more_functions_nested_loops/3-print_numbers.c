413d7fc445b631fcfc28029274def806e187abfd
Julianeme <juliantve@gmail.com>
2021-02-11 18:28:07 +0000

    prints 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints 0-9
+ *
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		_putchar(x);
+	}
+	_putchar(10);
+}
