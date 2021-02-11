b432c24507827b5162026c3d5f2a345b56dd453e
Julianeme <juliantve@gmail.com>
2021-02-11 22:35:45 +0000

    prints 1-14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,25 +9,16 @@ void more_numbers(void)
 {
 	int x;
 	int y;
-	int z;
 
 	for (x = 0; x <= 9; x++)
 	{
-		for (y = 48; y <= 57; y++)
+		for (y = 0; y <= 14; y++)
 		{
-			if (y <= 57)
+			if (y >= 10)
 			{
-			_putchar(y);
+				_putchar((y / 10) + 48);
 			}
-			if (y == 57)
-			{
-				for (z = 48; z <= 52; z++)
-				{
-				_putchar(49);
-				_putchar(z);
-				}
-
-			}
+			_putchar((y % 10) + 48);
 		}
 		_putchar(10);
 	}
