697b3f049a8944c134b77886fa4583d60013b412
Julianeme <juliantve@gmail.com>
2021-02-11 21:32:36 +0000

    prints a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_square - prints a square
+ *
+ *@n: int, the size of the square
+ */
+
+void print_diagonal(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		for (x = n; x < 0; x++)
+		{
+			for (y = 0; y < n; y++)
+			{
+				_putchar(35);
+			}
+			_putchar(10);
+		}
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
