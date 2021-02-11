9c5b45fbad908692701d844386d5d5f3ee81e1ff
Julianeme <juliantve@gmail.com>
2021-02-11 18:35:52 +0000

    print 0-9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints 0-9 except 2 and 4
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		if (x != 2 || x != 4)
+		_putchar(x);
+	}
+	_putchar(10);
+}
