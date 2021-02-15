34cdd5392ff93fec38c409eef51131005556a012
Julianeme <juliantve@gmail.com>
2021-02-15 22:11:29 +0000

    resets a variable to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98  - function that resets the value stored in a int to 98
+ *
+ *@n: pointer to the value of whats stored in the input value
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
