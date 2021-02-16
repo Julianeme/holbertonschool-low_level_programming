1bb0f3217f65f95206b288506f5414582a71bd3e
Julianeme <juliantve@gmail.com>
2021-02-16 15:39:39 +0000

    length function added

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,26 @@
 #include "holberton.h"
 
 /**
+ * _strlen - Returns the lengh of a string
+ *
+ *@s: string to be evaluated
+ *Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	return (a);
+}
+
+#include "holberton.h"
+
+/**
  * _puts - Prints a string
  *
  *@str: string to be printed
@@ -8,7 +28,9 @@
 
 void _puts(char *str)
 {
-	x = strlen (str);
+	int x, i;
+
+	x = strlen(str);
 	for (i = 0; i <= x; i++)
 	{
 		_putchar(str[i]);
