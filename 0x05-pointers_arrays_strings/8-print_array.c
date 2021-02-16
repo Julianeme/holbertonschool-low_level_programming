2630ccd189371f4ee9a8d94b17bbc087c82cc63a
Julianeme <juliantve@gmail.com>
2021-02-16 20:06:39 +0000

    code correction

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,14 +12,14 @@ void print_array(int *a, int n)
 
 {
 	int x;
- 
-		for (x = 0; x < n; x++)
+
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", a[x]);
+		if (x < (n - 1))
 		{
-			printf("%d", a[x]);
-			if (x < (n - 1))
-			{
-				printf(", ");
-			}
+			printf(", ");
 		}
-		printf("\n");
+	}
+	printf("\n");
 }
