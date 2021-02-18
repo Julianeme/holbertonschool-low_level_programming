e8871186fb6a48a0674a5a5e14451c586136221d
Julianeme <juliantve@gmail.com>
2021-02-18 18:06:05 +0000

    copies a string until n bytes

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copy src to dest
+ *
+ *@dest: string to be copied on
+ *@src: string to be copied from
+ *@n: number of bytes to be copied
+ *Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x = 0;
+
+	while (x < n)
+	{
+		dest[x] = src[x];
+		x++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
