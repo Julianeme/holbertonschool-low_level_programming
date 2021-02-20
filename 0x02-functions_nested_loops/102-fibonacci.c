548064b6b18af3ac8c15ee115d414c4fd80dc4a0
Julianeme <juliantve@gmail.com>
2021-02-20 16:19:26 +0000

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - descripción: prints first 50 fib numbers
+ *
+ *Return: always 0
+ */
+
+void main(void)
+
+{
+	unsigned int a = 0;
+	unsigned int b = 1;
+	int x;
+	unsigned int fib;
+
+	for (x = 0; x <= 50; x++)
+	{
+		fib = a + b;
+		a = b;
+		b = fib;
+		printf("%u", fib);
+			if (x < 50)
+			{
+				printf(", ");
+			}
+	}
+	printf("\n");
+}
