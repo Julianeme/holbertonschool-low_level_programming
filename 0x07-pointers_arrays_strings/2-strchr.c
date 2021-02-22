2267ea2c413081c2e6c4c146a48967e57cc8c87b
Julianeme <juliantve@gmail.com>
2021-02-22 18:43:13 +0000

    locate character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ *_strchr - searches for c in s
+ *@s: string where the c char will be looked for
+ *@c: char to be looked for in s
+ *Return: pointer to location of c
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s+i);
+		}
+	}
+	if (s[i] == c)
+	{
+		return (s+i);
+	}
+	else
+	{
+		return ('\0');
+	}
+}
