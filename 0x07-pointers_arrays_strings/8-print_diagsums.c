e398a4f12d396ac6e1ef5eb54e519b7b0cc5de38
Julianeme <juliantve@gmail.com>
2021-02-23 01:59:24 +0000

    sum diagonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ *print_diagsums - Prints the sum of the diagonals
+ *@a: input matrix
+ *@size: size of the matrix
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int j;
+	int sum1 = 0, sum2 = 0;
+	int pos1 = 0, pos2 = 0;
+
+	for (j = 0; j < size; j++)
+	{
+		pos1 = j * (size + 1);
+		pos2 = j * (size -1);
+		sum1 = sum1 + a[pos1];
+		sum2 = sum2 + a[pos2];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
