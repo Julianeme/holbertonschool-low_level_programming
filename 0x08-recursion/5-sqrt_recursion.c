c0a084304f99a2063ae2af788ce473bd281d8b98
Julianeme <juliantve@gmail.com>
2021-02-25 19:02:57 +0000

    code correction for big numbers

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,54 +1,54 @@
 #include "holberton.h"
+int auxiliar(int res, int m);
 
 /**
  * auxiliar - isolates the value of n to compare
  *@res: decreasing value starting from n
  *@m: saves the value of n
- *Return: square root of n
+ *Return: square root of n for positive numbers
+ *
  */
 
 int auxiliar(int res, int m)
 {
 	int resultado;
 
-if ((res * res) != m && res > 0)
-{
-	resultado = 1 + auxiliar(res - 1, m);
-}
-else
-{
-	return (0);
-}
-return (resultado);
+	if ((res * res) == m)
+	{
+		resultado = res;
+	}
+	else if ((res * res) > m)
+	{
+		return (-1);
+	}
+	else
+	{
+		resultado = (auxiliar(res + 1, m));
+	}
+	return (resultado);
 }
 
-#include "holberton.h"
-
- /**
-  * _sqrt_recursion - returns the square root of n
-  *@n: int
-  *Return: square root of n
-  */
+/**
+ * _sqrt_recursion - returns the square root of n
+ *@n: int
+ *Return: square root of n
+ */
 
 int _sqrt_recursion(int n)
 {
-	int res1 = n;
+	int res1 = 1;
 
 	if (n < 0)
 	{
 		return (-1);
 	}
-	if (n == 1)
+	if (n == 0)
 	{
-		return (1);
+		return (0);
 	}
-	else /*isolates n to be able to comparer*/
+	else
 	{
-		res1 = n - auxiliar(res1, n);
-		if (res1 == 0)
-		{
-			return (-1);
-		}
+		res1 = auxiliar(res1, n);
 	}
 	return (res1);
 }
