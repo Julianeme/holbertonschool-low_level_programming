91ff68271017b7371b2cff8b9d7e42efdf67a70d
Julianeme <juliantve@gmail.com>
2021-03-02 20:03:00 +0000

    copies a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - duplicates an array
+ *@str: array to be copied
+ *Return: 0 - NULL if unsuccesuf, otherwise pointer to copied array
+ */
+
+char *_strdup(char *str)
+{
+
+	unsigned int i;
+	unsigned int a = 0;
+	char *array;
+
+	while (str[a] != '\0')
+		a++;
+	if (a == 0)
+		return (NULL);
+	array = (char *)malloc(a * sizeof(char));
+	if (array == 0)
+		return (NULL);
+	for (i = 0; i < a; i++)
+	{
+		*(array + i) = *(str + i);
+	}
+	return (array);
+}
