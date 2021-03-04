c8ccb32efb3c93d843c454f3a654056d97ffb8b6
Julianeme <juliantve@gmail.com>
2021-03-04 03:34:43 +0000

    concatenates the arguments of program

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -13,30 +13,35 @@ char *argstostr(int ac, char **av)
 	int length = 0;
 	char *array;
 	int i, j;
+	int k = 0;
 
-	if (ac < 1)
+	if (ac < 1 || av == NULL)
 	{
 		return (NULL);
 	}
-	else
-	{
-		for (i = 1; i <= ac; i++)
-		{
-			for (j = 0; av[i][j] != '\0'; j++)
-			{
-				length = length + 1;
-			}
-		}
-	array = malloc(length * sizeof(char));
-	}
-	if (array == 0)
-		return (NULL);
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			array[i + j] = *av[i + j];
+			length = length + 1;
 		}
+		length = length + 1;
+	}
+	length = length + 1;
+	array = malloc(length * sizeof(char));
+	if (array == 0)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			array[k] = av[i][j];
+			k++;
+		}
+		array[k] = '\n';
+		k++;
+		if (i == ac)
+			array[k] = '\0';
 	}
 return (array);
 }
