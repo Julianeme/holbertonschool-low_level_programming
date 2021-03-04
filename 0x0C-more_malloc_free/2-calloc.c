6264f3e04a1167fb427f9edb6837ec6a5efecfd5
Julianeme <juliantve@gmail.com>
2021-03-04 22:34:16 +0000

    creates a string

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ *_calloc - creates a string of nmemb long and size int
+ *@nmemb: number of elements
+ *@size: size
+ *Return: a pointer to the tring
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *array;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	array = malloc(nmemb * size);
+	if (array == 0)
+		return (NULL);
+	for (i = 0; i < (size * nmemb); i++)
+		array[i] = 0;
+return (array);
+}
