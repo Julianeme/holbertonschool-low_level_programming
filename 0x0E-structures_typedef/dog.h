513ad5374237210aecf628dec19b13e308526d0f
Julianeme <juliantve@gmail.com>
2021-03-08 23:47:17 +0000

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - creates an structure for dog, age and owner name
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H */
