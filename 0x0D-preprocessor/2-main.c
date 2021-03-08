369e77f61589dab5f26c7adc7399ba558c812fc7
Julianeme <juliantve@gmail.com>
2021-03-08 19:06:43 +0000

    print its name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- *main - prints 6th argument it receives
+ *main - prints its name
  *Return: always 0
  */
 
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
