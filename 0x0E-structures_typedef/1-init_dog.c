de7a6d496d4dd8d0e4ebca5931b889270af07205
Julianeme <juliantve@gmail.com>
2021-03-09 01:05:49 +0000

    initializes a struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializes structure for dog
+ * @d: pointer to dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+}
