f7337e4908a94ece9e3b3700044a4469e7e5af22
Julianeme <juliantve@gmail.com>
2021-03-09 01:54:26 +0000

    prints dog strat

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - initializes structure for dog
+ * @d: pointer to dog struct
+ */
+
+void print_dog(struct dog *d)
+{
+
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age < 0)
+		printf("Age: (nil)");
+	else
+		printf("Age: %1f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+		printf("Owner: %s\n", d->owner);
+}
