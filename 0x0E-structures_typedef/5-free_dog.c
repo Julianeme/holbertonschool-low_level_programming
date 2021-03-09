c2a34c2c57528290fe2c8a7d58dbc28c4b64e028
Julianeme <juliantve@gmail.com>
2021-03-09 04:55:58 +0000

    frees new dog memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free the memory reserved for copies new_dog funct
+ * @d: the copied struct
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
