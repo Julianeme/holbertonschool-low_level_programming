98afbfcf82325e9bc6d5f475f12fb35a2414e05a
Julianeme <juliantve@gmail.com>
2021-03-11 03:34:40 +0000

    operations prototypes

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,16 @@
+/**
+ * op_add - adds a + b
+ * @a: int
+ * @b: int
+ * Return: a+b.
+ */
+int op_add(int a, int b)
+{ return (a + b); }
+int op_sub(int a, int b)
+{ return (a - b); }
+int op_mul(int a, int b)
+{ return (a * b); }
+int op_div(int a, int b)
+{ return (a / b); }
+int op_mod(int a, int b)
+{ return (a % b); }
