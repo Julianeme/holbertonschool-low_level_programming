109a6cfcc4da975c9491042e85e845c5acc71506
Julianeme <juliantve@gmail.com>
2021-03-11 05:15:45 +0000

    operations functions

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -6,11 +6,35 @@
  */
 int op_add(int a, int b)
 { return (a + b); }
+/**
+ * op_sub - bus b from a
+ * @a: int
+ * @b: int
+ * Return: a-b.
+ */
 int op_sub(int a, int b)
 { return (a - b); }
+/**
+ * op_mul - multiplies a times b
+ * @a: int
+ * @b: int
+ * Return: a*b.
+ */
 int op_mul(int a, int b)
 { return (a * b); }
+/**
+ * op_div - divides a between b
+ * @a: int
+ * @b: int
+ * Return: a/b.
+ */
 int op_div(int a, int b)
 { return (a / b); }
+/**
+ * op_mod - returns the module of the division of a between b
+ * @a: int
+ * @b: int
+ * Return: a%b.
+ */
 int op_mod(int a, int b)
 { return (a % b); }
