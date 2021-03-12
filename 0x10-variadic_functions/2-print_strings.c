ed25a27bac21cfeb973d6f7f802db9abfe7a5545
Julianeme <juliantve@gmail.com>
2021-03-12 02:42:27 +0000

    prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,20 +15,20 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list words;
 
 	va_start(words, n);
-
-		while (i < n)
+	if (n == 0)
+		return;
+	while (i < n)
+	{
+		if (words == NULL)
+			printf("(nil)");
+		else
 		{
-
-			if (words == NULL)
-				printf("(nil)");
-			else
-			{
-				printf("%s", va_arg(words, char *));
-				if (i != (n - 1) && separator != NULL)
-					printf("%s", separator);
-			}
-			i++;
+			printf("%s", va_arg(words, char *));
+			if (i != (n - 1) && separator != NULL)
+				printf("%s", separator);
 		}
+		i++;
+	}
 	printf("\n");
 	va_end(words);
 }
