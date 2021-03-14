dd0222bb95112d76d90fea9acc097231a78995c7
Julianeme <juliantve@gmail.com>
2021-03-14 17:38:43 +0000

    struct added

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,8 +3,20 @@
 
 #include <stdarg.h>
 
+typedef struct prt
+{
+	char *prt;
+	void (constant unsigned int n, ...);
+} _print;
+
+void prt_num(const unsigned int n, ...)
+void prt_str(const unsigned int n, ...)
+void prt_fl(const unsigned int n, ...)
+void prt_char(const unsigned int n, ...)
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_float(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
