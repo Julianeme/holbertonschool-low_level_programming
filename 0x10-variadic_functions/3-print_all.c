02b663fb35c26d71078073f0afd114ef25990453
Julianeme <juliantve@gmail.com>
2021-03-14 17:27:05 +0000

    functions creation

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -38,9 +38,52 @@ void print_str(const unsigned int n, ...)
 	va_start(words, n);
 	while (i < n)
 	{
-		printf("%s ", s);
+		printf("%s ", va_arg(words, char*);
 		i++;
 	}
 printf("\n");
 va_end(words);
+}
+
+/**
+ * print_fl - prints float numbers
+ *@n: number of arguments
+ */
+
+
+void print_fl(const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list flnum;
+
+	va_start(flnum, n);
+	while (i < n)
+	{
+		printf("%.2f ", va_arg(flnum, int));
+		i++;
+	}
+}
+printf("\n");
+va_end(flnum);
+}
+
+/**
+ * print_char - prints chars
+ *@n: number of arguments
+ */
+
+
+void print_char(const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list ch;
+
+	va_start(ch, n);
+	while (i < n)
+	{
+		printf("%c ", va_arg(ch, char*);
+		i++;
+	}
+	printf("\n");
+	va_end(ch);
 }
