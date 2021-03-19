abd9f459405ff4d83d6759524157729cbcd7ac29
Julianeme <juliantve@gmail.com>
2021-03-19 15:29:35 +0000

    funcion description added

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_list -  prints all the elements of a list_t list
+ * list_len -  returns the number of nodes in a liked list
  * @h: a pointer to the element of the list
  * Return: the number of nodes
  */
