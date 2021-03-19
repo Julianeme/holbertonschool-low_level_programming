4e5cd8f94d5563b034abb5639f2daaec9ee690c5
Julianeme <juliantve@gmail.com>
2021-03-19 15:10:14 +0000

    stdio.h library added

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_list -  prints all the elements of a list_t list
@@ -13,12 +14,16 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (!(h->str))
-			_printf("[0] (nill)\n");
+		{
+			printf("[0] (nill)\n");
+			h = h->next;
+			nod_count++;
+		}
 		else
 		{
-		_printf("[%i] %s\n", h->len, h->str);
-		h = h->next;
-		nod_count++;
+			printf("[%i] %s\n", h->len, h->str);
+			h = h->next;
+			nod_count++;
 		}
 	}
 return (nod_count);
