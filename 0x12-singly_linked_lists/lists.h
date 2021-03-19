c6914ba344daff612ba09cef71fb6c7cb01ea555
Julianeme <juliantve@gmail.com>
2021-03-19 20:03:30 +0000

    string library added

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -20,30 +21,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/**
- * struct prt - structure for printing various types
- *
- * @dt: The type of argument
- * @f: The function pointer to the print function
- */
-
-typedef struct prt
-{
-	char *dt;
-	int (*f)(va_list);
-} _print;
-
-int _printf(const char *format, ...);
-
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-int print_s(va_list arguments);
-int print_c(va_list arguments);
-int _printf(const char *format, ...);
-int _putchar(char c);
-int print_i(va_list m);
 #endif
