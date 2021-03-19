48d5dfef7f46057eb04e9672f0223e4e2190bfa3
Julianeme <juliantve@gmail.com>
2021-03-19 15:13:36 +0000

    error on declaration of function corrected

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -33,11 +33,13 @@ typedef struct prt
 	int (*f)(va_list);
 } _print;
 
+int _printf(const char *format, ...);
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);void free_list(list_t *head);
+void free_list(list_t *head);
 
 int print_s(va_list arguments);
 int print_c(va_list arguments);
