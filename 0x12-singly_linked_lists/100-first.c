38603b381ce900386330f0b42058cb7c0232e904
Julianeme <juliantve@gmail.com>
2021-03-20 03:59:29 +0000

    uploading function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * myStartupFun - executes a code before main is executed
+ */
+
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+I bore my house upon my back!\n'");
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("(A tortoise, having pretty good sense of a hare's nature,
+challenges one to a race.)\n");
+	return (0);
+}
