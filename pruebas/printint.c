220f854b3b298a44aa4951ffb293c3e530dcf2db
Julianeme <juliantve@gmail.com>
2021-03-20 23:05:42 -0500

    uploading function

---
+++ pruebas/printint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void print(int n);
+
+void print(int n)
+{
+  if (n < 0) {
+    putchar('-');
+    n = -n;
+  }
+
+  if (n/10)
+    print(n/10);
+
+  putchar(n%10 + '0');
+}
+
+int main()
+{
+  int n = -2147483647;
+  print(n);
+  return 0;
+}
