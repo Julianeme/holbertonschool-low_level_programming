f997f0b746909a8a0fc07e8713f5a4bae8aa7069
Julianeme <juliantve@gmail.com>
2021-03-25 17:44:48 -0500

    uploading file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
  * print_binary - convert and prints a binary tumber into decimal ui
  * @n: The number to be converted into binary
  *Return: number in binary
  */
-
-void print_binary(unsigned long int n)
+	void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
-	putchar((48 + (n & 1)));
+	_putchar((48 + (n & 1)));
 }
