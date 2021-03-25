4634140bf1e87abfa080961021de1265e29e70d1
Julianeme <juliantve@gmail.com>
2021-03-25 18:39:32 -0500

    uploading file

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * get_bit - returns the bit of a specific postion (index)
+ * @n: The number to be converted into binary
+ * @index: the binary requested position
+ *Return: number in binary
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (((n >> (index - 1)) && 1) == 1)
+		return (1);
+	return (0);
+}
