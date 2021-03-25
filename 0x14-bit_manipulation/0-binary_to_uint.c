90d67483acc58e288a33a8fc234a5b3308f401ef
Julianeme <juliantve@gmail.com>
2021-03-25 15:38:05 -0500

    core correction to travel the array

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
 
+/**
+ * binary_to_uint - convert and prints a binary tumber into decimal ui
+ * @b: The number to be printed in char format
+ *Return: the number in decimal ui format if success or 0 otherwise
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-  int strl = 0, i = 0, j =0;
-  unsigned int res = 0;
+	int i = 0, k, j;
+	unsigned int res, sum = 0;
 
-  while(b[i])
-  {
-	  if (b[i] != '1' && b[i] != '0')
-	  	return (0);
-	  i++;
-  }
-  while(j <= i)
-  {
-  	if (b[i] == 1)
-	  {
-		  res = res x 2
-	  }
-  }
-  print_number(res);
+	while (b[i])
+	{
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+		i++;
+	}
+	i = i - 1;
+	if (b[i] == '1')
+		sum = 1;
+	else if (b[i] == '0')
+		sum = 0;
+	i--;
+	k = i;
+	j = k;
+	while (i >= 0)
+	{
+		res = 1;
+		if (b[i] == '1')
+		{
+			j = i;
+			while (j <= k)
+			{
+				res = res * 2;
+				j++;
+			}
+			sum = sum + res;
+		}
+	i--;
+	}
+	return (sum);
 }
