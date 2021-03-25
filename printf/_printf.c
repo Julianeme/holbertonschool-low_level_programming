bfdd0443767cbe3cb97a09a9552dc1ba389a4943
Julianeme <juliantve@gmail.com>
2021-03-25 18:27:44 +0000

    uploading files for fist time

---
+++ printf/_printf.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _printf - ARGUMENT(s) according to FORMAT, or execute according to OPTION
+ * @format: entry format defined by user
+ * Return: amount of printed characters
+ */
+int _printf(const char *format, ...)
+{
+	_print print_any[] = {{"c", print_c}, {"s", print_s}, {"d", print_i},
+		{"i", print_i}, {00, 00}};
+	va_list arguments;
+	unsigned int x = 0, i = 0, count = 0;
+
+	va_start(arguments, format);
+	if (format == 00)
+		return (-1);
+	while (format && format[x])
+	{
+		if (format[x] != '%')
+		{
+			write(1, (format + x), 1);
+			count++;
+		}
+		if (format[x] == '%' && format[x + 1] == '%')
+		{
+			write(1, (format + x), 1);
+			count++;
+		}
+		if (format[x] == '%' && format[x + 1] != 00)
+		{
+			i = 0;
+			while (print_any[i].dt)
+			{
+				if (*print_any[i].dt == format[x + 1])
+					count += print_any[i].f(arguments);
+				i++;
+			}
+			x++;
+		}
+		x++;
+	}
+	va_end(arguments);
+	return (count);
+}
---
+++ printf/print_c.c
@@ -0,0 +1,79 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * print_c - prints chars
+ * @arguments: chart to print
+ * Return: number of character printed
+ */
+int print_c(va_list arguments)
+{
+	char str = va_arg(arguments, char*);
+
+	if (str == 00)
+	{
+		return (0);
+	}
+	write(1, &str, 1);
+	return (1);
+}
+/**
+ * print_s - prints strings
+ * @arguments: va list
+ * Return: number of character printed
+ */
+int print_s(va_list arguments)
+{
+	int i = 0;
+	char *str = va_arg(arguments, char*);
+
+	if (str == 00)
+	{
+		return (0);
+	}
+	while (str[i])
+	{
+		write(1, (str + i), 1);
+		i++;
+	}
+	return (i);
+}
+/**
+ *print_i - print an int
+ *@arguments: va list
+ *Return: number of character printed
+ */
+int print_i(va_list arguments)
+{
+	int m = va_arg(arguments, int);
+	int a = 1000000000;
+	int res;
+	int c = 0;
+	int cs = 1;
+	int count = 0;
+
+	if (m < 0)
+	{
+		_putchar(45);
+		cs = -1;
+		count++;
+	}
+	while (a >= 10)
+	{
+		res = (m / a) * cs;
+		if (res > 0)
+		{
+			c = 1;
+		}
+		if (c == 1)
+		{
+			_putchar(res + 48);
+			count++;
+		}
+		m = m % a;
+		a = a / 10;
+	}
+	m = m * cs;
+	_putchar((m % 10) + 48);
+	count++;
+	return (count);
+}
