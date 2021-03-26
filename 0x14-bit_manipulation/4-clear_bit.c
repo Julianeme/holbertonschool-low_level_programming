6e153f672ae916afcc19b71d01bdee5611c99bb2
Julianeme <juliantve@gmail.com>
2021-03-25 22:16:49 -0500

    code format correction

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * set_bit - sets the bit of a specific postion (index) to 1
+ * clear_bit - sets the bit of a specific postion (index) to 0
  * @n: The given number
  * @index: the binary requested position
  *Return: 1 on success, otherwise -1
@@ -11,7 +11,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 31)
 	return (-1);
-	*n ^= (1 << index);
-	/*n[index] = n[index] & 0;*/
+	n[index] = n[index] & 0;
 	return (1);
 }
