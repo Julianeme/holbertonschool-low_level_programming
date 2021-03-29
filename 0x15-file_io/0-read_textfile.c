48b31a7bd5b8d43e37122519d3cf30712f354698
Julianeme <juliantve@gmail.com>
2021-03-29 15:06:14 -0500

    Uploading Readme file, Header: holberton.h, and program to read from a fiel and write in the stou

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a text file and prints it
+ * to the POSIX standard output.
+ * @filename: pointer to the file to be readed
+ * @letters: number of letters it should read and print
+ * Return: number of letters it could read and print or 0 if fails
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, lr, lw;
+	char *rbuffer;
+
+	if (filename == NULL)
+		return (0);
+	rbuffer = malloc(sizeof(size_t) * letters);
+	if (rbuffer == NULL)
+	{
+	return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	lr = read(fd, rbuffer, letters);
+	if (lr == -1)
+	{
+		return (0);
+		free(rbuffer);
+	}
+	lw = write(STDOUT_FILENO, rbuffer, letters);
+	if (lw == -1)
+		return (0);
+	if (lw != lr)
+		return (0);
+	close(fd);
+	return (lw);
+}
