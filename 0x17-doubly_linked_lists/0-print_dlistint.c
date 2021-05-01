c47418e77207ef6fa025f80fe1437488ff0a739b
Julianeme <juliantve@gmail.com>
2021-05-01 14:06:32 -0500

    verification for empty node removed

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,23 +10,14 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int nod_count = 0;
+	dlistint_t *temp = *h;
 
 	if (!h)
 		return (0);
-	while (h != NULL)
-	{
-		if (!(h->n))
-		{
-			printf("0 (nil)\n");
-			h = h->next;
-			nod_count++;
-		}
-		else
-		{
-			printf("%i\n", h->n);
-			h = h->next;
-			nod_count++;
-		}
+	while (temp != NULL)
+	{	printf("%i\n", temp->n);
+		temp = temp->next;
+		nod_count++;
 	}
-return (nod_count);
+	return (nod_count);
 }
