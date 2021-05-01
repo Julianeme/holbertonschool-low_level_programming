61109a5201dc36f5a047e01093f327c1bca94acb
Julianeme <juliantve@gmail.com>
2021-05-01 14:20:02 -0500

    verification for empty node modified

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -15,18 +15,9 @@ size_t print_dlistint(const dlistint_t *h)
 		return (0);
 	while (h)
 	{
-		if (!(h->n))
-		{
-			printf("(nil)\n");
-			h = h->next;
-			nod_count++;
-		}
-		else
-		{
-			printf("%i\n", h->n);
-			h = h->next;
-			nod_count++;
-		}
+		printf("%i\n", h->n);
+		h = h->next;
+		nod_count++;
 	}
 	return (nod_count);
 }
