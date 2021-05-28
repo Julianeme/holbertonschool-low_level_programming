1222f87755c296722f93b52451d8ae04237c1347
Julianeme <juliantve@gmail.com>
2021-05-28 10:20:34 -0500

    betty style correction

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new = NULL;
 	char *copy_key = NULL;
 
-	if (!key)
+	if (!key || strcmp(key, "") == 0)
 		return (0);
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
