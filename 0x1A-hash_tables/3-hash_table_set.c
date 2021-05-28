b535a40139b091bb494326b489aa2559405f77c2
Julianeme <juliantve@gmail.com>
2021-05-28 10:47:26 -0500

    malloc failure return statement changed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
-		return (0);
+		return (NULL);
 	copy_key = strdup(key);
 	new->key = copy_key;
 	new->value = strdup(value);
