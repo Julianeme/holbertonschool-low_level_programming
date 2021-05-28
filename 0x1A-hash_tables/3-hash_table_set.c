de36839307758a4ec3d7a02b390db9d3f926e154
Julianeme <juliantve@gmail.com>
2021-05-28 14:12:01 -0500

    value validation

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new = NULL;
 	char *copy_key = NULL;
 
-	if (!ht || !key || !ht->array || ht->size <= 0)
+	if (!ht || !key || !ht->array || ht->size <= 0 || !value)
 		return (0);
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
