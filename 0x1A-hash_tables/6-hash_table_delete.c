7c3351c04b698a3372eebba6a7fc43076f06762a
Julianeme <juliantve@gmail.com>
2021-05-28 15:39:51 -0500

    hash table creation node fuction updated

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -11,38 +11,21 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i = 0;
 	hash_node_t *temp = NULL;
 
-	if (!ht)
-	{
-		while (i < ht->size)
-		{
-			free(ht->array[i]);
-			i++;
-		}
-		free(ht->array);
-		free(ht);
-		return;
-	}
+
 	while (i < ht->size)
 	{
-		if (!ht->array[i])
+		while(ht->array[i])
 		{
-			free(ht->array[i]);
-		}
-		else
-		{
-			temp = ht->array[i]->next;
-			while (ht->array[i])
-			{
+				temp = ht->array[i]->next;
 				free(ht->array[i]->key);
 				free(ht->array[i]->value);
 				free(ht->array[i]);
 				ht->array[i] = temp;
-				if (ht->array[i]->next)
-					temp = ht->array[i]->next;
-			}
 		}
 		i++;
 	}
 	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
 	free(ht);
 }
