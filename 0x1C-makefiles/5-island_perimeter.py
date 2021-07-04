f997db8749e6b9dcf6c88bef89c49adb5af3550e
Julianeme <juliantve@gmail.com>
2021-07-03 19:56:31 -0500

    conditional added to count perimeter if island is on the edges

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-returns the perimeter of the island described in grid
-
+Defines the funtion that measures the
+the perimeter of the island described in grid
 """
 
 
@@ -12,15 +12,29 @@ def island_perimeter(grid):
     fil = len(grid)
     col = len(grid[fil - 1])
     perimeter = 0
-    for f in range(fil):
-        for c in range(col):
-            if grid[f][c] == 1:
-                if f > 0 and grid[f - 1][c] == 0:
-                    perimeter += 1
-                if f < fil - 1 and grid[f + 1][c] == 0:
-                    perimeter += 1
-                if c > 0 and grid[f][c - 1] == 0:
-                    perimeter += 1
-                if c < col - 1 and grid[f][c + 1] == 0:
-                    perimeter += 1
+
+    if fil == 1 and col == 1 and grid[0][0] == 1:
+        return 1
+    else:
+        for f in range(fil):
+            for c in range(col):
+                if grid[f][c] == 1:
+                    if f > 0 and grid[f - 1][c] == 0:
+                        perimeter += 1
+                    if f < fil - 1 and grid[f + 1][c] == 0:
+                        perimeter += 1
+                    if c > 0 and grid[f][c - 1] == 0 or c == 0:
+                        perimeter += 1
+                    if c < col - 1 and grid[f][c + 1] == 0:
+                        perimeter += 1
+                    if f == 0:
+                        perimeter += 1
+                    if f == fil - 1:
+                        perimeter += 1
+                    if c == 0:
+                        perimeter += 1
+                    if c == col - 1:
+                        perimeter += 1
+                    if fil == 1:
+                        perimeter += 1
     return perimeter
